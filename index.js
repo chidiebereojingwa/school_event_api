61aa36a5b65be7847770e06f8658e298c59b8c36
chidiebereojingwa <ojingwaoji@gmail.com>
2021-12-20 07:50:30 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json())
-app.use("/api/user",userRoute);
+app.use("/api/users",userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/events", eventRoute);
 
